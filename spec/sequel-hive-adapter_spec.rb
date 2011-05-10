1aa0a5ad3149dd1a6aa1ecfd2feac1ea086185db
hrp <hrparmar@gmail.com>
2011-05-09 17:53:56 -0700

    clean up tests

--- spec/sequel-hive-adapter_spec.rb
+++ spec/sequel-hive-adapter_spec.rb
@@ -1,8 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SequelHiveAdapter" do
+
+  before(:all) do
+    TEST_HOST = "localhost"
+    @testdb = Sequel.connect("hive://#{TEST_HOST}")
+    @testtable = 'test_test_test'
+  end
+
   it "should load" do
-    Sequel.connect("hive://localhost")
-    #  fail "hey buddy, you should probably rename this file and start specing for real"
+    @testdb.should be_an_instance_of Sequel::Hive::Database
+  end
+
+  it "should convert string columns to strings" do
+  end
+
+  it "should handle group and count" do
   end
 end
